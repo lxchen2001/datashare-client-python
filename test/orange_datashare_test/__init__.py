bf2d5af8f2b30bd3800c9e53eeb263eb44f78cdf
Ben Einaudi <benjamin.einaudi@orange.com>
2017-02-17 11:00:07 +0100

    Add licence header

--- test/orange_datashare_test/__init__.py
+++ test/orange_datashare_test/__init__.py
@@ -1,3 +1,9 @@
+"""
+Copyright (C) 2016-2017 Orange
+This software is distributed under the terms and conditions of the 'BSD 3'
+license which can be found in the file 'LICENSE' in this package distribution
+"""
+
 import sys
 
 if sys.version_info.major == 2:
